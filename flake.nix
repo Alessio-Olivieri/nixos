25f0e865cc2f39357872ec8e11bb9fa950ddb2fd
Alessio-Olivieri <lexyo.2002@gmail.com>
2024-08-28 15:45:47 +0200

    e0

--- flake.nix
+++ flake.nix
@@ -1,17 +1,26 @@
 {
-  description = "A very basic flake";
+  description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-stable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-     outputs = { self, nixpkgs, ... }@inputs: {
-    # Please replace nixos with your hostname
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
         ./configuration.nix
+        inputs.home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.lexyo = import ./home.nix;
+        }
       ];
     };
   };
