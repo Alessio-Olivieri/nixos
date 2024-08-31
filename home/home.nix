1bcdf55fb415b5389309b3d9d9b87b63a01e6dfe
Alessio-Olivieri <lexyo.2002@gmial.com>
2024-08-31 15:21:04 +0200

    gnome extensions

--- home/home.nix
+++ home/home.nix
@@ -15,48 +15,15 @@
   gnome-manager.enable = true;
   vscode-module.enable = true;
 
-  # link the configuration file in current directory to the specified location in home directory
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # link all files in `./scripts` to `~/.config/i3/scripts`
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
-  # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
-    # here is some command line tools I use frequently
-    # feel free to add your own or remove some of them
 
     neofetch
-    nnn # terminal file manager
 
     # archives
     zip
     xz
     unzip
     p7zip
-
-    # utils
-    ripgrep # recursively searches directories for a regex pattern
-    fzf # A command-line fuzzy finder
-
-    # networking tools
-    mtr # A network diagnostic tool
-    iperf3
-    dnsutils  # `dig` + `nslookup`
-    ldns # replacement of `dig`, it provide the command `drill`
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
-    ipcalc  # it is a calculator for the IPv4/v6 addresses
 
     # misc
     cowsay
@@ -79,18 +46,13 @@
     iotop # io monitoring
     iftop # network monitoring
 
-    # system call monitoring
-    strace # system call monitoring
-    ltrace # library call monitoring
-    lsof # list open files
-
     # system tools
     sysstat
     lm_sensors # for `sensors` command
     ethtool
     pciutils # lspci
     usbutils # lsusb
-    wev
+    wev # wayland input
   ];
 
   # basic configuration of git, please change to your own
@@ -98,18 +60,6 @@
     enable = true;
     userName = "Alessio-Olivieri";
     userEmail = "lexyo.2002@gmial.com";
-  };
-
-  # starship - an customizable prompt for any shell
-  programs.starship = {
-    enable = true;
-    # custom settings
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
-    };
   };
 
   programs.bash = {
--- modules/gnome.nix
+++ modules/gnome.nix
@@ -9,5 +9,10 @@
         services.xserver.enable = true;
         services.xserver.displayManager.gdm.enable = true;
         services.xserver.desktopManager.gnome.enable = true;
+
+        environment.systemPackages = with pkgs.gnomeExtensions; [
+            blur-my-shell
+            pop-shell
+        ]
     };
 }
