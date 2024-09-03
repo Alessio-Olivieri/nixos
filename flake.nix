7be991a30e7c8c7874fc7c0c5da7c5398cb612e6
Alessio-Olivieri <lexyo.2002@gmial.com>
2024-09-03 18:38:12 +0200

    removed anyrun

--- flake.nix
+++ flake.nix
@@ -15,31 +15,21 @@
       inputs.home-manager.follows = "home-manager";
     };
 
-    # Adding the anyrun flake
-    anyrun = {
-      url = "github:anyrun-org/anyrun";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    #rose-pine-hyprcursor.url = "github:ndom91/rose-pine-hyprcursor";
     zen-browser.url = "github:MarceColl/zen-browser-flake";
   };
 
-  outputs = { self, nixpkgs, home-manager, plasma-manager, anyrun, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, plasma-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-
-        inputs.home-manager.nixosModules.home-manager{
+        inputs.home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.sharedModules = [plasma-manager.homeManagerModules.plasma-manager];
-          home-manager.users.lexyo = import home/home.nix;
+          home-manager.users.lexyo = import ./home/home.nix;
         }
-
       ];
-      specialArgs = { inherit inputs; };
     };
   };
 }
