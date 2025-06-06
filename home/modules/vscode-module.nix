37dfca5816a2ede18d14eb4a8588ed2546258ea5
Alessio-Olivieri <lexyo.2002@gmail.com>
2025-06-06 12:47:34 +0200

    added vscode settings.json

--- home/modules/vscode-module.nix
+++ home/modules/vscode-module.nix
@@ -1,21 +1,52 @@
 { pkgs, lib, config, ... }:
 {
-    options = {
-        vscode-module.enable = lib.mkEnableOption "Enables vscode";
-    };
+  options = {
+    vscode-module.enable = lib.mkEnableOption "Enables vscode";
+  };
 
-    config = lib.mkIf config.vscode-module.enable {
-          programs.vscode = {
-            enable = true;
-            profiles.default = {
-              extensions = with pkgs.vscode-extensions; [
-                ms-python.python
-                ms-python.debugpy
-                bbenoist.nix
-                ms-vscode-remote.remote-ssh
-                yzhang.markdown-all-in-one
-              ];
-            };
+  config = lib.mkIf config.vscode-module.enable {
+    programs.vscode = {
+      enable = true;
+      profiles.default = {
+        extensions = with pkgs.vscode-extensions; [
+          bbenoist.nix
+          ms-python.debugpy
+          ms-python.python
+          ms-vscode-remote.remote-ssh
+          yzhang.markdown-all-in-one
+        ];
+        userSettings = {
+          ### Editor & UI ###
+          "editor.minimap.enabled" = false;
+          "window.autoDetectColorScheme" = true;
+          "window.titleBarStyle" = "custom";
+          "workbench.colorTheme" = "Catppuccin Frappé";
+          "workbench.iconTheme" = "catppuccin-latte";
+          "workbench.preferredDarkColorTheme" = "Catppuccin Frappé";
+          "workbench.preferredLightColorTheme" = "Catppuccin Latte";
+
+          ### Explorer (File Tree) ###
+          "explorer.confirmDelete" = false;
+          "explorer.confirmDragAndDrop" = false;
+          "explorer.confirmPasteNative" = false;
+
+          ### Git ###
+          "git.enableSmartCommit" = true;
+          "git.ignoreMissingGitWarning" = true;
+
+          ### Python ###
+          "python.analysis.extraPaths" = [
+            "source"
+            "../source"
+          ];
+
+          ### Remote - SSH ###
+          "remote.SSH.showLoginTerminal" = true;
+
+          ### Terminal ###
+          "terminal.integrated.enableMultiLinePasteWarning" = false;
         };
+      };
     };
+  };
 }
