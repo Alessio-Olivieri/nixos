a289156f5cc195fbaa1a4dbc4dcd2ec515e00d79
Alessio-Olivieri <lexyo.2002@gmail.com>
2026-07-30 09:36:57 +0200

    removed delayed lid suspend

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
       ./modules/lutris.nix
       ./modules/virtualbox.nix
       ./modules/battery-background-policy.nix
-      ./modules/delayed-lid-suspend.nix
+      # ./modules/delayed-lid-suspend.nix
     ];
     gnome.enable = true;
     ai-module = {
